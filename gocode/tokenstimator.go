9029c2489608f1f93c3c6abd33e50190eb9b01f3
Ken King'ori <kkingori@gmail.com>
2018-02-14 08:52:33 +0300

    February 2018 update

--- gocode/tokenstimator.go
+++ gocode/tokenstimator.go
@@ -132,13 +132,13 @@ func unitsReqCost(unitsReq float64) (unitsReqCost float64) {
 
 //This function calculates the various charges and levies.
 func charges(unitsReq float64, unitsCharge float64) (float64, float64, float64, float64, float64, float64, float64, float64) {
-	const fcc float64 = 2.85
+	const fcc float64 = 4.35
 	fuelCharge := fcc * unitsReq
 	
-	const ferfa float64 = 2.35
+	const ferfa float64 = 1.16
 	forexCharge := ferfa * unitsReq
 	
-	const wrma float64 = 0.0129
+	const wrma float64 = 0.0203
 	wrmaCharge := wrma * unitsReq
 	
 	const inflation float64 = 0.39
@@ -170,13 +170,13 @@ func charges(unitsReq float64, unitsCharge float64) (float64, float64, float64, 
 
 //This function calculates the various charges and levies.
 func subsequentCharges(unitsReq float64, unitsCharge float64) (float64, float64, float64, float64, float64, float64, float64, float64) {
-	const fcc float64 = 2.85
+	const fcc float64 = 4.35
 	fuelCharge := fcc * unitsReq
 	
-	const ferfa float64 = 2.35
+	const ferfa float64 = 1.16
 	forexCharge := ferfa * unitsReq
 	
-	const wrma float64 = 0.0129
+	const wrma float64 = 0.0203
 	wrmaCharge := wrma * unitsReq
 	
 	const inflation float64 = 0.39
